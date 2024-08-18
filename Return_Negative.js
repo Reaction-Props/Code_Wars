e04fb8334df75eb0752f526f6d9319714758c6e8
React-Props <daniel.linker@intellicode-solutions.de>
2024-08-18 14:25:03 +0200

    Return Negative

--- Return_Negative.js
+++ Return_Negative.js
@@ -1,3 +1,19 @@
+//Aufgabe: "Return Negative"
+// Beschreibung:
+// In dieser Aufgabe bekommst du eine Zahl und sollst sie, falls sie positiv ist, in eine negative Zahl umwandeln. Falls die Zahl bereits negativ ist, gib sie unverändert zurück.
+//
+// Beispiel:
+//
+// makeNegative(1) gibt -1 zurück.
+// makeNegative(-5) gibt -5 zurück.
+// makeNegative(0) gibt 0 zurück.
+// Hinweise:
+//
+// Die Eingabe kann jede Ganzzahl sein.
+// Du darfst keine Multiplikation verwenden, um das Vorzeichen zu ändern.
+// Wie würdest du an diese Aufgabe herangehen?
+
+
 function makeNegative(num) {
     if (num > 0) {
         return -num;
